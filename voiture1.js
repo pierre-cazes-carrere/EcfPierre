62a75b85a58303c2cd3bd5c008c3bb1388efe8c1
Riep13 <pierrecazescarrere@gmail.com>
2023-09-10 02:11:41 +0200

    front

---
+++ voiture1.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const anneeSelect = document.getElementById("annee");
+    const kilometrageSelect = document.getElementById("kilometrage");
+    const prixSelect = document.getElementById("prix");
+    const vehicules = document.querySelectorAll(".vehicule");
+  
+    function filtrerVehicules() {
+      const anneeFiltre = anneeSelect.value;
+      const kilometrageFiltre = kilometrageSelect.value;
+      const prixFiltre = prixSelect.value;
+  
+      vehicules.forEach(function (vehicule) {
+        const anneeVehicule = vehicule.querySelector(".annee").textContent.split(": ")[1];
+        const kilometrageVehicule = vehicule.querySelector(".kilometre").textContent.split(": ")[1];
+        const prixFichuVehicule = vehicule.querySelector(".prix").textContent.split(": ")[1].replace("€", "").replace(/ /g, "");
+        const prixFichu = parseFloat(prixFichuVehicule);
+  
+        const doitAfficher = (anneeFiltre === "toutes" || anneeVehicule === anneeFiltre) &&
+          (kilometrageFiltre === "tous" || (kilometrageFiltre === "moins-50000" && parseFloat(kilometrageVehicule) < 50000) ||
+            (kilometrageFiltre === "50000-70000" && parseFloat(kilometrageVehicule) >= 50000 && parseFloat(kilometrageVehicule) <= 70000) ||
+            (kilometrageFiltre === "70000-100000" && parseFloat(kilometrageVehicule) >= 70000 && parseFloat(kilometrageVehicule) <= 100000) ||
+            (kilometrageFiltre === "100000-150000" && parseFloat(kilometrageVehicule) >= 100000 && parseFloat(kilometrageVehicule) <= 150000) ||
+            (kilometrageFiltre === "150000-plus" && parseFloat(kilometrageVehicule) >= 150000)) &&
+          (prixFiltre === "tous" || (prixFiltre === "1000-plus" && prixFichu >= 1000) ||
+            (prixFiltre === "3000-plus" && prixFichu >= 3000) ||
+            (prixFiltre === "6000-plus" && prixFichu >= 6000) ||
+            (prixFiltre === "10000-plus" && prixFichu >= 10000) ||
+            (prixFiltre === "15000-plus" && prixFichu >= 15000));
+  
+        if (doitAfficher) {
+          vehicule.style.display = "block";
+        } else {
+          vehicule.style.display = "none";
+        }
+      });
+    }
+  
+    anneeSelect.addEventListener("change", filtrerVehicules);
+    kilometrageSelect.addEventListener("change", filtrerVehicules);
+    prixSelect.addEventListener("change", filtrerVehicules);
+  });
+  
+
+
